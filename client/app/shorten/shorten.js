d45695788153ca58eeb2393990e61694607ce631
Chris Saden <csaden@gmail.com>
2015-07-31 15:57:53 -0700

    added ShortenController and link and addLink properties for controller

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,16 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
+  $scope.link = {};
+
+  $scope.addLink = function () {
+    Links.addLink()
+      .then(function(link) {
+        $scope.link = link;
+        $location.path('/links');
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
 });
