f05c9d68453f5f7a5071a1d7ca19091331e0bacf
Chris Saden <csaden@gmail.com>
2015-07-31 15:56:32 -0700

    added LinksController and data and getLinks properties

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,15 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+  $scope.data = {};
+
+  $scope.getLinks = function () {
+    Links.getLinks()
+      .then(function(links) {
+        $scope.data = links;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 });
